f27d164ff16be9e0cb00579cf16b7fd2ff222a6d
Maarten <merken.maarten@gmail.com>
2020-04-30 09:19:27 +0200

    WIP

---
+++ Translation.Plugins.Api/Translation.Plugin.Contract/ITranslationPlugin.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+
+namespace Translation.Plugin.Contract
+{
+    public class TranslationLanguage
+    {
+        public string LanguageCode { get; set; }
+        public string LanguageDescription { get; set; }
+    }
+
+    public class TranslationRequest
+    {
+        public string Text { get; set; }
+        public TranslationLanguage SourceLanguage { get; set; }
+        public TranslationLanguage TargetLanguage { get; set; }
+    }
+
+    public class TranslationResponse
+    {
+        public string OriginalText { get; set; }
+        public string TranslatedText { get; set; }
+        public decimal Accuracy { get; set; }
+        public TranslationLanguage SourceLanguage { get; set; }
+        public TranslationLanguage TargetLanguage { get; set; }
+
+    }
+
+    public interface ITranslationPlugin
+    {
+        Task<TranslationResponse> Translate(TranslationRequest request);
+    }
+}
---
+++ Translation.Plugins.Api/Translation.Plugin.Klingon/KlingonTranslationPlugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Translation.Plugin.Contract;
+
+namespace Translation.Plugin.Klingon
+{
+    public class KlingonTranslationPlugin : ITranslationPlugin
+    {
+        public async Task<TranslationResponse> Translate(TranslationRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
